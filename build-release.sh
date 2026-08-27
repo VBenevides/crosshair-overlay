e7752e95578b5284a307da9e1bb9eeb4e483cc26
Vinicius Benevides <massaki1999@gmail.com>
2026-08-27 16:50:34 -0300

    include try_build

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-set -eu
+set -u
 
 cd "$(dirname "$0")"
 
-cargo build --release --target x86_64-unknown-linux-gnu
-cargo build --release --target x86_64-pc-windows-gnu
+failures=
+
+try_build() {
+    name=$1
+    target=$2
+    if ! cargo build --release --target "$target"; then
+        failures="$failures $name"
+    fi
+}
+
+try_build linux x86_64-unknown-linux-gnu
+try_build windows x86_64-pc-windows-gnu
+
+if [ -n "$failures" ]; then
+    printf 'Failed builds:%s\n' "$failures"
+    exit 1
+fi
